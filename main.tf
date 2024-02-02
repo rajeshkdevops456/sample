f7da0712807f42ad4bec22ee0aaf52d45f985b9e
rajeshkdevops456 <149505134+rajeshkdevops456@users.noreply.github.com>
2024-02-02 22:56:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,22 +13,3 @@ resource "aws_instance" "rajesh_instance" {
     }
     
 }
-
-resource "aws_ec2_instance_state" "test" {
-  inorostance_id = aws_instance.rajesh_instance.id
-  state       = "stopped"
-}
-
-resource "aws_instance" "ip-machine" {
-  # Creates four identical aws ec2 instances
-  count = 4     
-  
-  # All four instances will have the same ami and instance_type
-  ami = lookup(var.ec2_ami,var.region) 
-  instance_type = "t2.micro" 
-  tags = {
-    # The count.index allows you to launch a resource 
-    # starting with the distinct index number 0 and corresponding to this instance.
-    Name = "my-machine-${count.index}"
-  }
-}
